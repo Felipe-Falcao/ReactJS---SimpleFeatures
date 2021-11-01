dd6fe616866cba5002982109449e61f3d0ca5abe
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-10-31 21:45:10 -0300

    feat: create and export login page

---
+++ src/presentation/pages/index.js
@@ -0,0 +1,4 @@
+import Login from './login'
+import Topics from './topics'
+
+export { Login, Topics }
---
+++ src/presentation/pages/login/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { GeneralStyles } from '../../styles'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Login</h2>
+      </div>
+    );
+  }
+}
+
+export default Login;
